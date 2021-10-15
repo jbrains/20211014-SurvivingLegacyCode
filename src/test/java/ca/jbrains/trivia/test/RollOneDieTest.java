d68f52ffa15ef6d11d76235727e0ccf0876117b5
J. B. Rainsberger <me@jbrains.ca>
2021-10-15 09:26:10 -0300

    [FAILING TEST] Tried to write a test and it already failed before an assertion.

---
+++ src/test/java/ca/jbrains/trivia/test/RollOneDieTest.java
@@ -0,0 +1,12 @@
+package ca.jbrains.trivia.test;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.Test;
+
+public class RollOneDieTest {
+    @Test
+    public void iDoNotKnowYet() {
+        Game game = new Game();
+        game.roll(4);
+    }
+}
