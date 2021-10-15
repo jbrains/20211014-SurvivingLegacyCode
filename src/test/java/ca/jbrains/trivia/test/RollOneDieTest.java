8e3788567516ba1edb9cd8bee03533f773a83756
J. B. Rainsberger <me@jbrains.ca>
2021-10-15 09:37:38 -0300

    Added a boundary case for moving the player around the board: just before going back to the starting place.

--- src/test/java/ca/jbrains/trivia/test/RollOneDieTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollOneDieTest.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 
 public class RollOneDieTest {
     @Test
-    public void currentPlayerMovesCorrectly() {
+    public void currentPlayerMovesCorrectly_Four() {
         TestableGame game = new TestableGame();
         game.add("::player 1::");
         game.roll(4);
         Assert.assertEquals(4, game.getPlaceOfCurrentPlayer());
+    }
+
+    @Test
+    public void currentPlayerMovesCorrectly_Eleven() {
+        TestableGame game = new TestableGame();
+        game.add("::player 1::");
+        game.roll(11);
+        Assert.assertEquals(11, game.getPlaceOfCurrentPlayer());
     }
 }
